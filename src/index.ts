bee74083c7f5191da4774e1039ac2b6c70583935
Matt McElwee <matt.mcelwee@jointhebibleproject.com>
2022-08-10 22:09:39 -0500

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,3 @@
+class Naughty {
+  constructor(level) {}
+}
---
+++ src/tags.ts
@@ -0,0 +1,16 @@
+const tags = {
+  tbp: 'taboo_body_part',
+  v: 'vulgar',
+  i: 'insult',
+  s: 'sexual',
+  h: 'homophobic',
+  r: 'racist',
+  t: 'toilet_humor',
+  m: 'misogynist',
+  n: 'nazi',
+  c: 'contextual',
+  a: 'animal_cruelty',
+  vi: 'violent',
+};
+
+export default tags;
